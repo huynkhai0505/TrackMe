d42f84991eb1108dea06c1066d194bc3fb54b254
huynhkhai0505 <huynhkhai0505@gmail.com>
2020-07-26 18:05:37 +0700

    DOC

--- web/public/app.js
+++ web/public/app.js
@@ -1,6 +1,7 @@
 $('#navbar').load('navbar.html');
 
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
+const users = JSON.parse(localStorage.getItem('users')) || [];
 
 devices.forEach(function(device) { 
     $('#devices tbody').append(`
@@ -9,6 +10,15 @@ devices.forEach(function(device) {
       <td>${device.name}</td>
     </tr>`
   ); });
+
+  //Iterate in userLogin
+users.forEach(function (user) { 
+  $('#users tbody').append(`
+  <tr>
+    <td>${user.user1}</td> 
+    <td>${user.password}</td>
+  </tr>`
+); });
 
   //Add device
 $('#add-device').on('click', function() { 
@@ -24,3 +34,46 @@ $('#add-device').on('click', function() {
         console.log(`command is: ${command}`);
 });
 
+//Register
+$('#register').on('click', function() { 
+  const user1 = $('#LoginName').val();
+  const password = $('#LoginPassword').val();
+  const confirmpassword = $('#confirmpassword').val();
+
+  const exists = users.find((user) => { 
+    return user.user1 === user1;
+  });
+
+  if(exists){
+    alert("This User Name has been registered");
+  }
+  else{
+    //push in localStorage
+    users.push ({ user1, password });
+    localStorage.setItem('users', JSON.stringify(users));
+    location.href = 'login.html'; 
+  }
+});
+
+//Login  
+
+$('#login').on('click', function() { 
+  const user2 = $('#user2').val();
+  const password2= $('#password2').val();
+
+  //Check existed
+  const existed = users.find(user => {
+    if( user.user1 === user2 && user.password === password2 ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+    if(existed) {
+    localStorage.setItem( 'isAuthenticated', true);
+    location.href = "/";
+  } else {
+      alert("Wrong Password or Username");
+    }
+});   
+  
--- web/server.js
+++ web/server.js
@@ -12,5 +12,12 @@ app.get('/send-command', (req, res) => { res.sendFile(`${base}/send-command.html
 });
 app.get('/about', (req, res) => { res.sendFile(`${base}/about-me.html`);
 });
+app.get('/registration', (req, res) => { res.sendFile(`${base}/registration.html`);
+});
+app.get('/login', (req, res) => { res.sendFile(`${base}/login.html`);
+});
+app.get('/device-history', (req, res) => { res.sendFile(`${base}/device-history.html`);
+});
+
 app.listen(port, () => { console.log(`listening on port ${port}`);
 });

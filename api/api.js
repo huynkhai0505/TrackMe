dd3a0cd1ce6a0bf461d8e7adb3cc4ad83e69ff69
huynhkhai0505 <huynhkhai0505@gmail.com>
2020-07-29 11:47:25 +0700

    DOC

--- api/api.js
+++ api/api.js
@@ -8,7 +8,10 @@ useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express');
 const app = express();
-const port = 5000;
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json())
+const port = process.env.PORT || 5000;
 
 app.get('/api/test', (req, res) => { res.send('The API is working!');
 });
@@ -19,6 +22,9 @@ app.get('/api/devices', (req, res) => { Device.find({}, (err, devices) => {
     return res.send(devices);
     }
 }); 
+});
+
+app.post('/api/devices', (req, res) => { console.log(req.body);
 });
 
 app.listen(port, () => { console.log(`listening on port ${port}`);

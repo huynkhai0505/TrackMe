8ffa6a0fa5de244a3efcae3f0bfb4d0d81bdbcba
huynhkhai0505 <huynhkhai0505@gmail.com>
2020-07-29 11:23:53 +0700

    DOC

--- api/api.js
+++ api/api.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://<dbuser>: <URLENCODEDdbpassword>@cluster0.dyvnr.mongodb.net', {
-    useNewUrlParser: true, useUnifiedTopology: true 
-});
-
 const Device = require('./models/device');
+
+mongoose.connect('mongodb+srv://huynhkhai1203:Quynhanh0505@cluster0.dyvnr.mongodb.net/test?retryWrites=true&w=majority'
+    , {useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 const express = require('express');
 const app = express();
@@ -13,10 +13,13 @@ const port = 5000;
 app.get('/api/test', (req, res) => { res.send('The API is working!');
 });
 
-app.get('/api/devices', (req, res) => { 
-    Device.find({}, (err, devices) => {
-    console.log(devices);
+app.get('/api/devices', (req, res) => { Device.find({}, (err, devices) => {
+    if (err == true) { return res.send(err);
+    } else {
+    return res.send(devices);
+    }
 }); 
 });
+
 app.listen(port, () => { console.log(`listening on port ${port}`);
 });

2663f8ac162dbe55d43dddb53dc79071908c0c38
huynhkhai0505 <huynhkhai0505@gmail.com>
2020-07-29 12:15:05 +0700

    DOC

--- api/api.js
+++ api/api.js
@@ -40,5 +40,10 @@ app.post('/api/devices', (req, res) => {
 app.post('/api/send-command', (req, res) => { console.log(req.body);
 });
 
+app.use(function(req, res, next) { 
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+    next(); });
+
 app.listen(port, () => { console.log(`listening on port ${port}`);
 });
--- web/server.js
+++ web/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const base = `${__dirname}/public`;
 const port = process.env.PORT || 3000;
 

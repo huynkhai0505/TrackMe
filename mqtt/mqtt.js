2663ed30d684f592cdc114e65ccccf54bc962ef0
huynhkhai0505 <huynhkhai0505@gmail.com>
2020-08-09 15:07:25 +0700

    DOC

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -3,4 +3,10 @@ const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
 
 client.on('connect', () => { 
     console.log('connected');
+    const topic = '/clientId-qFVX1JtWng/test/hello/'; 
+    const msg = 'Hello MQTT world!'; 
+    client.publish(topic, msg, () => {
+        console.log('message sent...'); 
+    });
 });
+
